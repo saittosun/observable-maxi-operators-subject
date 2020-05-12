4f9edaca648b60b8faab0552a9f1fd7405a2ff9d
tostos <saittosun2@gmail.com>
2020-05-12 18:01:46 +0200

    subject is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   userActivated = false;
+  private activatedSub: Subscription;
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe(didActivate => {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
       this.userActivated = didActivate;
     });
   }
+
+  // tslint:disable-next-line:max-line-length
+  // One important note, just as with your own observables, you should unsubscribe to your subjects though whenever you don't need them. So add onDestroy here to the app component
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
+  }
+
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -21,6 +21,10 @@ export class UserComponent implements OnInit {
   }
 
   onActivate() {
-    this.userService.activatedEmitter.emit(true);
+    // this.userService.activatedEmitter.emit(true);
+
+    // tslint:disable-next-line:max-line-length
+    // We know observables, we can subscribe to them as you learned but they're rather passive. You'll learn how to create your own observable but the core idea always is that you wrap a callback or an event or something like that. A subject is different, a subject also is an object you can subscribe to but it's more active because you can actively call next on it from outside. Remember in the observable, we also called next but that was from inside the observable when we created it. So that is a more active observable that is perfect when we want to use it as an event emitter, so if we don't have a passive event source, like an HTTP request or DOM events but if we have something that actively needs to be triggered by us in our application and that's exactly the use case we have here.
+    this.userService.activatedEmitter.next(true);
   }
 }
